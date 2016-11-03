de07d013b8dc92f210d21459d7c22fbc799e4525
sidjayisaok <sidjay18@gmail.com>
2016-11-03 16:19:57 -0400

    es6-ified code and made it DRY

--- Sniphy.js
+++ Sniphy.js
@@ -1,11 +1,42 @@
 //basic array
-var categories =[];
+    let categories =[];
+
+    const giphyReturn = (response)=>{
+  	//this part handles the gif logic
+                        let APIresults = response.data;
+                        let imageUrl = APIresults.image_url;
+                        let imageMP4 = APIresults.image_mp4_url
+                        let giphyImage = $("<img>");
+                        let p = $("<p>");
+
+                       giphyImage.attr('src', imageUrl);
+                       giphyImage.attr('alt', 'giphy image');
+                       $("#images").empty();
+                       $("#images").prepend(p);
+                       $('#images').prepend(giphyImage);
+                       $('#giphyURL').empty();
+                       $('#giphyURL').append(imageUrl);
+						//this part handles the MP4 logic
+                       $('#images').on('click', 'img', ()=>{
+                         let giphyVideo = $("<video>");
+                         giphyVideo.attr('src', imageMP4);
+                         giphyVideo.attr('type', 'video/MP4');
+                         giphyVideo.prop('autoplay', true);
+                         giphyVideo.prop('loop', true);
+                         giphyVideo.prop('controls', true);
+                         $("#images").empty();
+                         $("#images").prepend(p);
+                         $('#images').prepend(giphyVideo);
+                         $('#giphyURL').empty();
+                         $('#giphyURL').append(imageMP4);
+                       });
+}
 
 	  //this creates our buttons from search results
-      function makeButtons(){
+      const makeButtons = ()=> {
         $('#buttons').empty();
-        for(var i=0; i < categories.length; i++){
-          var buttons = $("<button>");
+        for(let i = 0; i < categories.length; i++){
+          let buttons = $("<button>");
           buttons.addClass('letter letter-button-color');
           buttons.data('let', categories[i]);
           buttons.text(categories[i]);
@@ -19,97 +50,41 @@ var categories =[];
       }
 
 	  //this executes our basic search via the form and button
-      $('#findGiphy').click(function(){
-
+      $('#findGiphy').click(()=>{
           search = $('#giphySearch').val();
           categories.push(search);
           //this returns a button based on the search result
 		  makeButtons();
 		  //setup the link for the API
-          var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + search;
+          const queryURL = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + search;
 		  //AJAX call
           $.ajax({
             url: queryURL,
             method: 'GET'
-          })
-			  //this adds a random gif/mp4 based on the search parameter
-              .done(function(response) {
-                  var APIresults = response.data;
-                  var imageUrl = APIresults.image_url;
-                  var imageMP4 = APIresults.image_mp4_url
-                  var giphyImage = $("<img>");
-                  var p = $("<p>");
-                  giphyImage.attr('src', imageUrl);
-                  giphyImage.attr('alt', 'giphy image');
-                  $("#images").empty();
-                  $("#images").prepend(p);
-                  $('#images').prepend(giphyImage);
-                  $('#giphyURL').empty();
-                  $('#giphyURL').append(imageUrl);
-				  //this part handles the mp4 when the image is clicked
-                  $('#images').on('click', 'img', function(){
-                    var giphyVideo = $("<video>");
-                    giphyVideo.attr('src', imageMP4);
-                    giphyVideo.attr('type', 'video/MP4');
-                    giphyVideo.prop('autoplay', true);
-                    giphyVideo.prop('loop', true);
-                    giphyVideo.prop('controls', true);
-                    $("#images").empty();
-                    $("#images").prepend(p);
-                    $('#images').prepend(giphyVideo);
-                    $('#giphyURL').empty();
-                    $('#giphyURL').append(imageMP4);
-                  });
-              });
+          }).done((response)=>{
+            giphyReturn(response);
+          });
               return false;
       });
 	  //this removes the search buttons
-      $('#clearGiphy').click(function(){
+      $('#clearGiphy').click(()=>{
         categories.remove();
         return false;
       });
-	  //this makes the buttons return a gif when clicking them
-      function workButtons(){
-        $('#buttons').unbind().on('click', 'button', function(){
-             var thisClick = $(this).text();
-             console.log(thisClick);
+	  //this makes the buttons return a gif when clicking them	
+      const workButtons = ()=>{
+        $('#buttons').unbind().on('click', 'button', ()=>{
+             let thisClick = $(this).text();
 			 //url for API
-             var clickURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + thisClick;
+             const clickURL = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + thisClick;
 
              $('#images').empty();
 
              $.ajax({url: clickURL,
                      method: 'GET'
-             })
-					//this part handles the gif logic
-                   .done(function(response){
-                     var APIresults = response.data;
-                       var imageUrl = APIresults.image_url;
-                       var imageMP4 = APIresults.image_mp4_url
-                       var giphyImage = $("<img>");
-                       var p = $("<p>");
-                       giphyImage.attr('src', imageUrl);
-                       giphyImage.attr('alt', 'giphy image');
-                       $("#images").empty();
-                       $("#images").prepend(p);
-                       $('#images').prepend(giphyImage);
-                       $('#giphyURL').empty();
-                       $('#giphyURL').append(imageUrl);
-						//this part handles the MP4 logic
-                       $('#images').on('click', 'img', function(){
-                         var giphyVideo = $("<video>");
-                         giphyVideo.attr('src', imageMP4);
-                         giphyVideo.attr('type', 'video/MP4');
-                         giphyVideo.prop('autoplay', true);
-                         giphyVideo.prop('loop', true);
-                         giphyVideo.prop('controls', true);
-                         $("#images").empty();
-                         $("#images").prepend(p);
-                         $('#images').prepend(giphyVideo);
-                         $('#giphyURL').empty();
-                         $('#giphyURL').append(imageMP4);
-                       });
-                   });
+             }).done((response)=>{
+               giphyReturn(response);
+             });
          });
       }
 	//function to execute the button
